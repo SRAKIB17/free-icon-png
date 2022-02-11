f24a682634933ac2b3948055cb1b4ff9587d4732
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-11 16:16:17 +0600

    ss

--- index.js
+++ index.js
@@ -6,37 +6,73 @@ function showDefault(){
 }
 function showIcon(page){
     
-    console.log(page)
     const icon = document.getElementById('icon');
-    for (let index = 1; index < page*100-1; index++) {
-        let divIcon = 
-            `<div id="icon-2" class="p-1 w-full rounded-sm  shadow-sm shadow-cyan-500 relative icon hover:shadow-blue-500 hover:shadow-md">
-                <div class=" bg-red-50 top-0 left-0 bottom-0  p-1 z-[-1]">
-                    <img src="images/128px/${index}.png" alt="" >
-                 </div>
-                <div class="absolute justify-between z-20 download">
-                    <div class="bg-violet-200 rounded-md hover:bg-violet-400">
-                        <a href="images/32px/${index}.png" class="z-40" download="">
-                            <i style="font-size:20px" class="fa">32px</i>
-                        </a>
-                    </div>
-                    <div class="bg-yellow-200 rounded-md hover:bg-yellow-400">
-                        <h3>
-                            <a href="images/64px/${index}.png" class="z-40" download="">
-                                <i style="font-size:20px" class="fa">64px</i>
+    if (page.value>1) {
+        let index = 100*(page.value-1)
+        console.log(index)
+        while (index < page.value*100-1) {
+            let divIcon = 
+                `<div id="icon-2" class="p-1 w-full rounded-sm  shadow-sm shadow-cyan-500 relative icon hover:shadow-blue-500 hover:shadow-md">
+                    <div class=" bg-red-50 top-0 left-0 bottom-0  p-1 z-[-1]">
+                        <img src="images/128px/${index}.png" alt="" >
+                     </div>
+                    <div class="absolute justify-between z-20 download">
+                        <div class="bg-violet-200 rounded-md hover:bg-violet-400">
+                            <a href="images/32px/${index}.png" class="z-40" download="">
+                                <i style="font-size:20px" class="fa">32px</i>
                             </a>
-                        </h3>
+                        </div>
+                        <div class="bg-yellow-200 rounded-md hover:bg-yellow-400">
+                            <h3>
+                                <a href="images/64px/${index}.png" class="z-40" download="">
+                                    <i style="font-size:20px" class="fa">64px</i>
+                                </a>
+                            </h3>
+                        </div>
+                        <div class="bg-green-200 hover:bg-green-400">
+                            <a href="images/128px/1.png" class="z-40" download="">
+                                <i style="font-size:24px" class="fa">&#xf019;</i>
+                            </a>
+                            
+                        </div>
+        
                     </div>
-                    <div class="bg-green-200 hover:bg-green-400">
-                        <a href="images/128px/1.png" class="z-40" download="">
-                            <i style="font-size:24px" class="fa">&#xf019;</i>
-                        </a>
-                        
+                </div>`
+            index++
+            icon.innerHTML += divIcon
+        }
+    }
+    else{
+        for (let index = 1; index < page*100-1; index++) {
+            let divIcon = 
+                `<div id="icon-2" class="p-1 w-full rounded-sm  shadow-sm shadow-cyan-500 relative icon hover:shadow-blue-500 hover:shadow-md">
+                    <div class=" bg-red-50 top-0 left-0 bottom-0  p-1 z-[-1]">
+                        <img src="images/128px/${index}.png" alt="" >
+                     </div>
+                    <div class="absolute justify-between z-20 download">
+                        <div class="bg-violet-200 rounded-md hover:bg-violet-400">
+                            <a href="images/32px/${index}.png" class="z-40" download="">
+                                <i style="font-size:20px" class="fa">32px</i>
+                            </a>
+                        </div>
+                        <div class="bg-yellow-200 rounded-md hover:bg-yellow-400">
+                            <h3>
+                                <a href="images/64px/${index}.png" class="z-40" download="">
+                                    <i style="font-size:20px" class="fa">64px</i>
+                                </a>
+                            </h3>
+                        </div>
+                        <div class="bg-green-200 hover:bg-green-400">
+                            <a href="images/128px/1.png" class="z-40" download="">
+                                <i style="font-size:24px" class="fa">&#xf019;</i>
+                            </a>
+                            
+                        </div>
+        
                     </div>
-    
-                </div>
-            </div>`
-    
-        icon.innerHTML += divIcon
+                </div>`
+        
+            icon.innerHTML += divIcon
+        }
     }
 }
--- page.js
+++ page.js
@@ -3,9 +3,9 @@
 const pageMain = document.getElementById('page')
 let page = 1;
 while (page <= 10){
-    let pageDiv = `<buttom class="bg-fuchsia-300 w-fit p-1 rounded-full hover:bg-fuchsia-400 border-solid border-2 hover:border-indigo-600 hover:bg-green" value="${page}" onclick='showIcon()'>
+    let pageDiv = `<button class="bg-fuchsia-300 w-fit p-1 rounded-full hover:bg-fuchsia-400 border-solid border-2 hover:border-indigo-600 hover:bg-green" value="${page}" id='page-${page}' onclick='showIcon(this)'>
     ${page}
-    </buttom>`
+    </button>`
     pageMain.innerHTML += pageDiv;
     page++
 }
