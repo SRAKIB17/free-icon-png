798378ae29d816009f1b6e07d1fabd36e6970736
SRAKIB17 <rakibulssc5@gmail.com>
2022-02-11 20:31:40 +0600

    ss

--- page.js
+++ page.js
@@ -1,20 +1,17 @@
 
 
-const pageMainTop = document.getElementById('pageTop')
-const pageMainBottom = document.getElementById('pageBottom')
+const pageMain = document.getElementById('page')
 let page = 1;
 function pageCount(perPage){
     let totalImage = 1000;
     let countPage = Math.round(totalImage/perPage);
     let page = 1;
-    pageMainBottom.innerHTML = ''
-    pageMainTop.innerHTML = ''
+    pageMain.innerHTML = ''
     while (page <= countPage){
         let pageDiv = `<button class="bg-fuchsia-300 w-fit p-1 rounded-full hover:bg-fuchsia-400 border-solid border-2 hover:border-indigo-600 hover:bg-green" value="${page}" id='page-${page}' onclick='showIcon(this)'>
         ${page}
         </button>`
-        pageMainTop.innerHTML += pageDiv;
-        pageMainBottom.innerHTML += pageDiv;
+        pageMain.innerHTML += pageDiv;
         page++
     }
 }
